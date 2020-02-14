4aa117ee75fdb7cc62f51c34b37af746db6dbf55
Victor de Farias Silva <victor-silva@victor.silva>
2020-02-14 16:31:24 -0300

    Consuming API to register clients

--- src/pages/NewClient.js
+++ src/pages/NewClient.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
+import { Typography, Button } from '@material-ui/core';
+import axios from '../utils/httpClient'
 
 const useStyles = makeStyles(theme => ({
     root: {
+        // marginTop: '150px',
+    },
+    form: {
         '& .MuiTextField-root': {
             margin: theme.spacing(2),
             width: 200,
@@ -11,7 +16,6 @@ const useStyles = makeStyles(theme => ({
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        margin: theme.spacing(3),
     },
     requiredAdvice: {
         marginTop: theme.spacing(2),
@@ -20,60 +24,60 @@ const useStyles = makeStyles(theme => ({
     }
 }));
 
-export default function NewClient() {
 
+const NewClient = (props) => {
     const classes = useStyles();
+    const {history} = props;
+    const [client, setClient] = useState({ name: "", phone: "" });
 
+    const post = async (event) => {
+        event.preventDefault();
+        async function postClients() {
+            await axios.post('/clients', client).then(()=>history.push("/novo-endereco"))
+        }
+        postClients();
+    }
+
+    const handleClientChange = (event) => {
+        let field = event.target.name
+        let value = event.target.value;
+        setClient({ ...client, [field]: value });
+    }
+
+    
     return (
-        <form className={classes.root} noValidate autoComplete="off">
-            <TextField
-                required
-                id="outlined-required"
-                label="Nome"
-                defaultValue=""
-                variant="outlined"
-            />
+        <>
+            <Typography className={classes.root} align='center' variant="h4">
+                Cadastro de Clientes
+            </Typography>
+            <form className={classes.form} noValidate onSubmit={post} >
 
-            
-            <TextField
-                required
-                id="outlined-required"
-                label="Telefone"
-                placeholder="(XX) XXXXX-XXXX"
-                variant="outlined"
-            />
-            <TextField
-                required
-                id="outlined-required"
-                label="Cidade"
-                defaultValue="Itápolis"
-                variant="outlined"
-            />
-            <TextField
-                required
-                id="outlined-required"
-                label="Bairro"
-                variant="outlined"
-            />
-            <TextField
-                required
-                id="outlined-required"
-                label="Rua"
-                defaultValue=""
-                variant="outlined"
-            />
-            <TextField
-                required
-                id="outlined-required"
-                label="Número"
-                variant="outlined"
-            />
-            <TextField
-                label="Complemento"
-                placeholder="Ex: Fundos"
-                variant="outlined"
-            />
-            <span className={classes.requiredAdvice}>Campos com * são obrigatórios </span>
-        </form>
+                <TextField
+                    required
+                    name="name"
+                    onChange={handleClientChange}
+                    defaultValue={client.name}
+                    id="outlined-required"
+                    label="Nome"
+                    variant="outlined"
+                />
+                <TextField
+                    required
+                    name="phone"
+                    value={client.phone}
+                    onChange={handleClientChange}
+                    id="outlined-required"
+                    label="Telefone"
+                    placeholder="(XX) XXXXX-XXXX"
+                    variant="outlined"
+                />
+                <span className={classes.requiredAdvice}>Campos com * são obrigatórios</span>
+                <Button type="submit">
+                    Enviar
+                </Button>
+            </form>
+        </>
     );
 }
+
+export default NewClient;
